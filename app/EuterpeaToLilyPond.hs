720b672579ada71bdbf3774645841e5ad3b43411
Jim Snavely <ludflu@gmail.com>
2025-03-22 20:33:55 -0400

    more

--- app/EuterpeaToLilyPond.hs
+++ app/EuterpeaToLilyPond.hs
@@ -1,69 +1,69 @@
 module EuterpeaToLilyPond (convertToLilyPond) where
 
-import Euterpea 
+import Euterpea qualified as E
 import Music.Lilypond qualified as LP
 import Data.Ratio ((%))
 import qualified Data.List as List
 
 -- | Converts Euterpea Music data to LilyPond Music data
-convertToLilyPond :: Music Pitch -> LP.Music
+convertToLilyPond :: E.Music E.Pitch -> LP.Music
 convertToLilyPond = musicToLilyPond
 
 -- | Main conversion function for Music values
-musicToLilyPond :: Music Pitch -> LP.Music
-musicToLilyPond (Prim (Note d p)) = noteToLilyPond d p
-musicToLilyPond (Prim (Rest d)) = restToLilyPond d
-musicToLilyPond (m1 :+: m2) = LP.Sequential [musicToLilyPond m1, musicToLilyPond m2]
-musicToLilyPond (m1 :=: m2) = LP.Simultaneous [musicToLilyPond m1, musicToLilyPond m2]
-musicToLilyPond (Modify c m) = modifyToLilyPond c (musicToLilyPond m)
+musicToLilyPond :: E.Music E.Pitch -> LP.Music
+musicToLilyPond (E.Prim (E.Note d p)) = noteToLilyPond d p
+musicToLilyPond (E.Prim (E.Rest d)) = restToLilyPond d
+musicToLilyPond (m1 E.:+: m2) = LP.Sequential [musicToLilyPond m1, musicToLilyPond m2]
+musicToLilyPond (m1 E.:=: m2) = LP.simultaneous (musicToLilyPond m1) (musicToLilyPond m2)
+musicToLilyPond (E.Modify c m) = modifyToLilyPond c (musicToLilyPond m)
 
 -- | Convert a note to LilyPond format
-noteToLilyPond :: Dur -> Pitch -> LP.Music
+noteToLilyPond :: E.Dur -> E.Pitch -> LP.Music
 noteToLilyPond d (pc, oct) = 
     LP.Note (pitchToLilyPond pc oct) (durationToLilyPond d) [] []
 
 -- | Convert a rest to LilyPond format
-restToLilyPond :: Dur -> LP.Music
+restToLilyPond :: E.Dur -> LP.Music
 restToLilyPond d = LP.Rest (durationToLilyPond d) [] []
 
 -- | Convert a pitch to LilyPond format
-pitchToLilyPond :: PitchClass -> Octave -> LP.Pitch
+pitchToLilyPond :: E.PitchClass -> E.Octave -> LP.Pitch
 pitchToLilyPond pc oct = 
     let noteName = case pc of
-            Cff -> LP.C
-            Cf -> LP.Cis
-            Dff -> LP.D
-            Css -> LP.Cis
-            C  -> LP.C
-            Cs -> LP.Cis
-            Df -> LP.Des
-            D  -> LP.D
-            Ds -> LP.Dis
-            Ef -> LP.Ees
-            Eff -> LP.Ees
-            E  -> LP.E
-            F  -> LP.F
-            Fs -> LP.Fis
-            Gf -> LP.Ges
-            G  -> LP.G
-            Gs -> LP.Gis
-            Af -> LP.Aes
-            A  -> LP.A
-            As -> LP.Ais
-            Bf -> LP.Bes
-            B  -> LP.B
+            E.Cff -> LP.C
+            E.Cf -> LP.Cis
+            E.Dff -> LP.D
+            E.Css -> LP.Cis
+            E.C  -> LP.C
+            E.Cs -> LP.Cis
+            E.Df -> LP.Des
+            E.D  -> LP.D
+            E.Ds -> LP.Dis
+            E.Ef -> LP.Ees
+            E.Eff -> LP.Ees
+            E.E  -> LP.E
+            E.F  -> LP.F
+            E.Fs -> LP.Fis
+            E.Gf -> LP.Ges
+            E.G  -> LP.G
+            E.Gs -> LP.Gis
+            E.Af -> LP.Aes
+            E.A  -> LP.A
+            E.As -> LP.Ais
+            E.Bf -> LP.Bes
+            E.B  -> LP.B
         -- Adjust octave (Euterpea uses different octave numbering than LilyPond)
         lilyOctave = oct - 4
     in LP.Pitch noteName Nothing lilyOctave
 
 -- | Convert duration to LilyPond format
-durationToLilyPond :: Dur -> LP.Duration
+durationToLilyPond :: E.Dur -> LP.Duration
 durationToLilyPond d =
     let (num, denom) = toRationalForm d
     in LP.Duration (fromIntegral num) (fromIntegral denom) Nothing []
 
 -- | Convert a rational duration to a simple numerator/denominator form
-toRationalForm :: Dur -> (Integer, Integer)
+toRationalForm :: E.Dur -> (Integer, Integer)
 toRationalForm d =
     let r = toRational d
         num = numerator r
@@ -71,45 +71,45 @@ toRationalForm d =
     in (num, denom)
 
 -- | Handle control modifications
-modifyToLilyPond :: Control -> LP.Music -> LP.Music
-modifyToLilyPond (Tempo r) m = 
+modifyToLilyPond :: E.Control -> LP.Music -> LP.Music
+modifyToLilyPond (E.Tempo r) m = 
     LP.Tempo (LP.TempoMM (fromRational r)) m
-modifyToLilyPond (Transpose i) m =
+modifyToLilyPond (E.Transpose i) m =
     LP.Transposition (fromIntegral i) m
-modifyToLilyPond (Instrument _) m = 
+modifyToLilyPond (E.Instrument _) m = 
     -- LilyPond handles instruments differently, would need to be
     -- implemented based on specific needs
     m
-modifyToLilyPond (KeySig pc mode) m =
+modifyToLilyPond (E.KeySig pc mode) m =
     let tonality = case mode of
-            Major -> LP.Major
-            Minor -> LP.Minor
+            E.Major -> LP.Major
+            E.Minor -> LP.Minor
         key = pitchClassToLilyPondKey pc
     in LP.KeySignature key tonality m
-modifyToLilyPond (Phrase attrs) m =
+modifyToLilyPond (E.Phrase attrs) m =
     foldr applyAttribute m attrs
   where
-    applyAttribute :: PhraseAttribute -> LP.Music -> LP.Music
-    applyAttribute (Dyn d) music = 
+    applyAttribute :: E.PhraseAttribute -> LP.Music -> LP.Music
+    applyAttribute (E.Dyn d) music = 
         let dynamic = case d of
                 -- Map Euterpea dynamics to LilyPond dynamics
                 -- These are approximations
-                SF  -> LP.SforzandoPiano
-                Ff  -> LP.ForteFortissimo
-                Fff -> LP.ForteFortissimo
-                F   -> LP.Forte
-                MF  -> LP.MezzoForte
-                MP  -> LP.MezzoPiano
-                P   -> LP.Piano
-                PP  -> LP.PianoPianissimo
-                PPP -> LP.PianoPianissimo
+                E.SF  -> LP.SforzandoPiano
+                E.Ff  -> LP.ForteFortissimo
+                E.Fff -> LP.ForteFortissimo
+                E.F   -> LP.Forte
+                E.MF  -> LP.MezzoForte
+                E.MP  -> LP.MezzoPiano
+                E.P   -> LP.Piano
+                E.PP  -> LP.PianoPianissimo
+                E.PPP -> LP.PianoPianissimo
         in LP.Dynamic dynamic music
-    applyAttribute (Art a) music =
+    applyAttribute (E.Art a) music =
         let articulation = case a of
-                Staccato _     -> LP.Staccato
-                Legato _       -> LP.Legato
-                Accent _       -> LP.Accent
-                Marcato       -> LP.Marcato
+                E.Staccato _     -> LP.Staccato
+                E.Legato _       -> LP.Legato
+                E.Accent _       -> LP.Accent
+                E.Marcato       -> LP.Marcato
                 _             -> LP.Accent  -- Default for others
         in LP.Articulation articulation music
     applyAttribute _ music = music  -- Default case for other attributes
@@ -117,20 +117,20 @@ modifyToLilyPond (Phrase attrs) m =
 -- | Convert PitchClass to LilyPond key
 -- pitchClassToLilyPondKey :: PitchClass -> LP.Key
 pitchClassToLilyPondKey pc = case pc of
-    C  -> LP.C
-    Cs -> LP.Cis
-    Df -> LP.Des
-    D  -> LP.D
-    Ds -> LP.Dis
-    Ef -> LP.Ees
-    E  -> LP.E
-    F  -> LP.F
-    Fs -> LP.Fis
-    Gf -> LP.Ges
-    G  -> LP.G
-    Gs -> LP.Gis
-    Af -> LP.Aes
-    A  -> LP.A
-    As -> LP.Ais
-    Bf -> LP.Bes
-    B  -> LP.B
+    E.C  -> LP.C
+    E.Cs -> LP.Cis
+    E.Df -> LP.Des
+    E.D  -> LP.D
+    E.Ds -> LP.Dis
+    E.Ef -> LP.Ees
+    E.E  -> LP.E
+    E.F  -> LP.F
+    E.Fs -> LP.Fis
+    E.Gf -> LP.Ges
+    E.G  -> LP.G
+    E.Gs -> LP.Gis
+    E.Af -> LP.Aes
+    E.A  -> LP.A
+    E.As -> LP.Ais
+    E.Bf -> LP.Bes
+    E.B  -> LP.B
